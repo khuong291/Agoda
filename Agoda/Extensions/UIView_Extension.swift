e4309c47aed6fc3dbb5246313a7dc04ae1233d8d
Khuong Pham <dkhuong291@gmail.com>
2016-06-07 13:11:03 +0700

    Create AgodaView

---
+++ Agoda/Extensions/UIView_Extension.swift
@@ -0,0 +1,16 @@
+//
+//  UIView_Extension.swift
+//  Agoda
+//
+//  Created by Khuong Pham on 6/7/16.
+//  Copyright © 2016 Fantageek. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    func makeViewCircle() {
+        self.layer.cornerRadius = self.frame.width / 2
+        self.clipsToBounds = true
+    }
+}
---
+++ Agoda/Views/AgodaView.swift
@@ -0,0 +1,44 @@
+//
+//  AgodaView.swift
+//  Agoda
+//
+//  Created by Khuong Pham on 6/7/16.
+//  Copyright © 2016 Fantageek. All rights reserved.
+//
+
+import UIKit
+
+class Agoda: UIView {
+    
+    @IBOutlet var contentView: UIView!
+    @IBOutlet var blueView: UIView!
+    @IBOutlet var purpleView: UIView!
+    @IBOutlet var greenView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var redView: UIView!
+    @IBOutlet var agodaLabel: UILabel!
+    @IBOutlet var stackView: UIStackView!
+
+    required override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initSubviews()
+    }
+
+    func initSubviews() {
+        let nib = UINib(nibName: "AgodaView", bundle: nil)
+        nib.instantiateWithOwner(self, options: nil)
+
+        blueView.makeViewCircle()
+        purpleView.makeViewCircle()
+        greenView.makeViewCircle()
+        redView.makeViewCircle()
+        yellowView.makeViewCircle()
+
+        contentView.frame = bounds
+        addSubview(contentView)
+    }
+}

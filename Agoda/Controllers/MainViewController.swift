0b6dab6c3a113e6397c0a40f45e54d1fb2497e5f
Khuong Pham <dkhuong291@gmail.com>
2016-06-07 17:48:20 +0700

    Finish Create SearchView

--- Agoda/Controllers/MainViewController.swift
+++ Agoda/Controllers/MainViewController.swift
@@ -17,6 +17,9 @@ class MainViewController: UIViewController {
     @IBOutlet var quantityView: UIView!
     @IBOutlet var infoView: UIView!
     @IBOutlet var darkView: UIView!
+    @IBOutlet var checkInView: CheckDateView!
+    @IBOutlet var checkOutView: CheckDateView!
+    @IBOutlet var searchButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,6 +32,28 @@ class MainViewController: UIViewController {
         checkDateView.makeViewCorner()
         quantityView.makeViewCorner()
         darkView.makeViewCorner()
+        searchButton.makeViewCorner()
+
+        // Create Data For CheckDateView
+        createDataForCheckOutView()
+        createDataForCheckInView()
+
+    }
+
+    // MARK: Create Data For CheckDateView
+
+    private func createDataForCheckInView() {
+        checkInView.checkInLabel.text = "Check-in"
+        checkInView.dateLabel.text = "06"
+        checkInView.dayLabel.text = "Mon"
+        checkInView.monthLabel.text = "Jun"
+    }
+
+    private func createDataForCheckOutView() {
+        checkOutView.checkInLabel.text = "Check-out"
+        checkOutView.dateLabel.text = "07"
+        checkOutView.dayLabel.text = "tue"
+        checkOutView.monthLabel.text = "Jun"
     }
 
 }

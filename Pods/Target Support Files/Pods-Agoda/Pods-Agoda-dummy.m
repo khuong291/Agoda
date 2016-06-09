805e8f595ed53756860668527cc699ddd5d2e78c
Khuong Pham <dkhuong291@gmail.com>
2016-06-09 23:36:25 +0700

    Add Cosmos (RateView)

---
+++ Pods/Target Support Files/Pods-Agoda/Pods-Agoda-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Agoda : NSObject
+@end
+@implementation PodsDummy_Pods_Agoda
+@end
---
+++ Pods/Target Support Files/Pods-Agoda/Pods-Agoda-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_AgodaVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_AgodaVersionString[];
+
